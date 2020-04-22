30719a5a3d9e06674a19ee86c62ca68e9510eb9d
parade岁月 <362941236@qq.com>
2020-04-22 11:54:39 +0800

    自定义下载目录

--- app/src/main/java/com/parade/storagedemo/MainActivity.kt
+++ app/src/main/java/com/parade/storagedemo/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.parade.storagedemo
 
-import android.app.usage.ExternalStorageStats
 import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
-import java.util.jar.Manifest
-import java.util.logging.Logger
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -63,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         Log.e(TAG, "external-file-path:${cachePath}")
 
         button9.setOnClickListener {
-            download();
+            download()
         }
 
         button10.setOnClickListener {
@@ -83,12 +77,15 @@ class MainActivity : AppCompatActivity() {
                 connection.connectTimeout = 8000
                 connection.readTimeout = 8000
                 val values = ContentValues()
+                //创建文件名
                 values.put(MediaStore.MediaColumns.DISPLAY_NAME, "12345.jpg")
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    //创建文件路径
                     values.put(
                         MediaStore.MediaColumns.RELATIVE_PATH,
-                        Environment.DIRECTORY_DOWNLOADS
+                        "${Environment.DIRECTORY_DOWNLOADS}/${packageName}/"
                     )
+                    //创建文件URI
                     val uri =
                         contentResolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values)
                     connection.inputStream.use { input ->
@@ -100,49 +97,6 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
-                }
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    private fun android101Download() {
-        thread {
-            try {
-                val url =
-                    URL("https://cdn.pixabay.com/photo/2020/04/02/11/20/fungus-4994622_960_720.jpg")
-                val connection = url.openConnection() as HttpURLConnection
-                connection.requestMethod = "GET"
-                connection.connectTimeout = 8000
-                connection.readTimeout = 8000
-                val inputStream = connection.inputStream
-                val bis = BufferedInputStream(inputStream)
-                val values = ContentValues()
-                values.put(MediaStore.MediaColumns.DISPLAY_NAME, "12345.jpg")
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    values.put(
-                        MediaStore.MediaColumns.RELATIVE_PATH,
-                        Environment.DIRECTORY_DOWNLOADS
-                    )
-                    val uri =
-                        contentResolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values)
-                    uri?.let {
-                        val outputStream = contentResolver.openOutputStream(it)
-                        outputStream?.let { out ->
-                            val bos = BufferedOutputStream(out)
-                            val buffer = ByteArray(1024)
-                            var bytes = bis.read(buffer)
-                            while (bytes >= 0) {
-                                bos.write(buffer, 0, bytes)
-                                bos.flush()
-                                bytes = bis.read(buffer)
-                            }
-                            bos.close()
-                        }
-                    }
-                    bis.close()
                 }
 
             } catch (e: Exception) {
